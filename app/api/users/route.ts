adebf89a1eae06521a5ee3d9218df22d7bf1b281
Mauro <mauromaio.dev@gmail.com>
2023-10-17 13:25:01 -0300

    feat: createSchema new object - role

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,25 +1,26 @@
-import joi from 'joi';
+import joi from "joi";
 
-import { usersRepo } from '_helpers/server';
-import { apiHandler } from '_helpers/server/api';
+import { usersRepo } from "_helpers/server";
+import { apiHandler } from "_helpers/server/api";
 
 module.exports = apiHandler({
-    GET: getAll,
-    POST: create
+  GET: getAll,
+  POST: create,
 });
 
 async function getAll() {
-    return await usersRepo.getAll();
+  return await usersRepo.getAll();
 }
 
 async function create(req: Request) {
-    const body = await req.json();
-    await usersRepo.create(body);
+  const body = await req.json();
+  await usersRepo.create(body);
 }
 
 create.schema = joi.object({
-    firstName: joi.string().required(),
-    lastName: joi.string().required(),
-    username: joi.string().required(),
-    password: joi.string().min(6).required(),
+  firstName: joi.string().required(),
+  lastName: joi.string().required(),
+  username: joi.string().required(),
+  password: joi.string().min(6).required(),
+  role: joi.string().required(),
 });
