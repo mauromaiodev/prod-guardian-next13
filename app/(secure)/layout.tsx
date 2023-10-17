7ddd9acf938664ad2d9ae28a73e53e3c14a53ce5
Mauro <mauromaio.dev@gmail.com>
2023-10-17 13:22:23 -0300

    feat: remoção do footer

--- app/(secure)/layout.tsx
+++ app/(secure)/layout.tsx
@@ -20,13 +20,6 @@ function Layout({ children }: { children: React.ReactNode }) {
       <div className="p-4">
         <div className="container">{children}</div>
       </div>
-      <div className="text-center bg-dark p-2">
-        <p>
-          <a href="https://mauromaio.vercel.app/" target="_blank">
-            Mauro Maio Dev - Projetos
-          </a>
-        </p>
-      </div>
     </div>
   );
 }
