7c2c0b1243519ec11f18c9bef183c017e81c7c49
Mauro <mauromaio.dev@gmail.com>
2023-10-17 11:28:55 -0300

    feat: layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,17 +10,7 @@ export default Layout;
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-
-        <div className="text-center mt-4">
-          <p>
-            <a href="https://mauromaio.vercel.app/" target="_blank">
-              Mauro Maio Dev - Projetos
-            </a>
-          </p>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
