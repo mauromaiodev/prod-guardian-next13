1f4a0c1a8f82c085faf0af0a1b5c7183cdedef44
Mauro <mauromaio.dev@gmail.com>
2023-10-24 17:49:21 -0300

    feat: update user service roles

--- app/_services/useUserService.ts
+++ app/_services/useUserService.ts
@@ -46,7 +46,7 @@ function useUserService(): IUserService {
       router.push("/account/login");
     },
     register: async (user) => {
-      user.role = "SUPPLIER";
+      user.role = "USER";
       try {
         await fetch.post("/api/account/register", user);
         alertService.success("Cadastro feito com sucesso!", true);
